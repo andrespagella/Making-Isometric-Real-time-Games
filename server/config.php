ecb4e9a8d04f3cba6061aa91a7a6af3b16250654
Andres Pagella <andres.pagella@gmail.com>
2011-05-26 17:47:11 -0300

    Moved the database connection details to a config.php file

---
+++ server/config.php
@@ -0,0 +1,11 @@
+<?php
+	// Database connection details
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'mygameuser');
+	define('DB_PASSWORD', 'game123');
+	define('DB_NAME', 'mygame');
+
+	// Grid size
+	define('GRID_X', 50);
+	define('GRID_Y', 50);
+?>
